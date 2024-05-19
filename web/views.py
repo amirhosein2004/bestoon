3c1c5305b273f84ae100387adc4dc95a15bd994f
amirhosein <amirhoosenbabai82@gmail.com>
2024-05-19 12:09:19 +0330

    income in models and some beautication

--- web/views.py
+++ web/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from json import JSONEncoder
 
-# Create your views here.
+def submit_expense(request):
+    """user submit an expense"""
+
+    print ("we are here")
+    print(request.POST)
+
+    return JsonResponse({
+        'status' : 'ok',
+    }, encoder=JSONEncoder)
